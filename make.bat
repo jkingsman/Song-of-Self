3fcd350885ca190a88c5b986f1569637ebb4e7de
Jack Kingsman <jack.kingsman@gmail.com>
2016-06-24 11:49:52 -0700

    Commit to sans design

--- make.bat
+++ make.bat
@@ -4,10 +4,17 @@ del /S /Q dist
 
 mkdir build_temp
 
-pdflatex -aux-directory=build_temp -output-directory=dist Simple.tex
-pdf2svg dist/Simple.pdf dist/Simple.svg
-call svgexport dist/Simple.svg build_temp/Simple-uncrushed.png 4096:
-pngcrush -reduce -rem allb build_temp/Simple-uncrushed.png dist/Simple.png
+pdflatex -aux-directory=build_temp -output-directory=dist Default.tex
+rem pdflatex -aux-directory=build_temp -output-directory=dist Sans.tex
+
+pdf2svg dist/Default.pdf dist/Default.svg
+rem pdf2svg dist/Sans.pdf dist/Sans.svg
+
+call svgexport dist/Default.svg build_temp/Default-uncrushed.png 4096:
+rem call svgexport dist/Sans.svg build_temp/Sans-uncrushed.png 4096:
+
+pngcrush -reduce -rem allb build_temp/Default-uncrushed.png dist/Default.png
+rem pngcrush -reduce -rem allb build_temp/Sans-uncrushed.png dist/Sans.png
 
 del /S /Q build_temp
 rmdir build_temp
