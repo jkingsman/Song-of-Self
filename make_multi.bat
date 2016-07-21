f1da4d2909a78f3c7ea3e8887d033af0a8935c7c
Jack Kingsman <jack.kingsman@gmail.com>
2016-07-20 23:57:49 -0700

    Update spacing in makefile

--- make_multi.bat
+++ make_multi.bat
@@ -9,7 +9,9 @@ for %%f in (src/*.tex) do (
 	echo Processing %%~nf
 	pdflatex -aux-directory=build_temp -output-directory=dist src/%%~nf.tex
 	mv dist/%%~nf.pdf dist/pdf-%%~nf.pdf
+	
 	pdf2svg dist/pdf-%%~nf.pdf dist/svg-%%~nf.svg
+	
 	call svgexport dist/svg-%%~nf.svg build_temp/%%~nf-uncrushed.png 4096:
 	pngcrush -reduce -rem allb build_temp/%%~nf-uncrushed.png dist/png-%%~nf.png
 )
